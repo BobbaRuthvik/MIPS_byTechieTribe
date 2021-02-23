e6a6b9de48e8af0ed3ac8bd163f3621e703c6a84
BobbaRuthvik <73455820+BobbaRuthvik@users.noreply.github.com>
2021-02-23 14:02:32 +0530

    Update 23/02/21 14:02

--- mipsDummy.cpp
+++ mipsDummy.cpp
@@ -10,7 +10,7 @@ class mipsSimulator {
 		string registerNames[32] = {"zero", "at", "v0", "v1", "a0", "a1", "a2", "a3", "t0", "t1", "t2", "t3", "t4", "t5", "t6", "t7", "s0", "s1", "s2", "s3", "s4", "s5", "s6", "s7", "t8", "t9", "k0", "k1", "gp", "sp", "fp", "ra"};
 		int registerValues[32] = {};    // empty brace kept to initialize with 0
 		string commandsList[6] = {"add", "sub", "jump", "bne", "lw", "sw"};			// Add, sub, jump, bne, lw, sw(modify accordingly)
-		vector<string> program;
+		vector<vector<string>> program;
 		int maxLength;					// I think it's related to size limit mentioned(Please Check)
 		int programCounter;				// to store current line
 		string current_command;			// points to current command line
@@ -30,12 +30,15 @@ class mipsSimulator {
 						cout << "File is too large" << endl;
 						exit(1);
 					}
-					program.push_back(lineInput);		// now entire program is stored line by line, no file business anymore
+					program.push_back(subVector(lineInput));		// now entire program is stored line by line, no file business anymore
 				}
 				file.close();
-				for (int i = 0; i < program.size(); i++) {
-					cout << program[i] << endl;
-				}
+				for(int i=0; i<program.size(); i++){
+                    for(int j=0; j<program[i].size(); j++){
+                        cout << program[i][j] << " ";
+                    }
+                    cout << endl;
+                }
 			}
 			else {
 				cout << "File does not exist" << endl;
@@ -43,8 +46,45 @@ class mipsSimulator {
 			}
 		}
 
+        vector<string> subVector(string s){
+            vector<string> v;
+            string temp = "";
+            for(int i=0; i<s.size(); i++){
+                if(s[i] == ',' || s[i] == '$'){
+                    continue;
+                }
+                else{
+                    temp += s[i];
+                }
+            }
+            return getWords(temp);  
+        }
+
+
+        vector<string> getWords(string temp){
+            vector<string> v;
+            string word = "";
+            for (auto x : temp)  
+            { 
+                if (x == ' ' || x == '\t') 
+                {
+                    if(word!="")
+                        v.push_back(word); 
+                    word = ""; 
+                } 
+                else { 
+                    word = word + x; 
+                } 
+            }
+            if(word!="") 
+                v.push_back(word); 
+            return v;
+        }
+
+// vector<vector<string>>
         void execute(){
             while(programCounter < commandCount){
+                // program vector loop, 
 				/*current_command = program[programCounter] */
                 //removeTabsSpaces(current_command);	// return current_command(trimmed)						
                 if(current_command == ""){
@@ -111,7 +151,7 @@ int main() {
 	cout << "MIPS Simulator" << endl;
 	// int option                  // for mode selection(Step by Step execution / Whole execution)
 	// we can also open file from here
-	ofstream myFile("myFile2.txt");
+	ofstream myFile("myFile3.txt");
 	if (!myFile.is_open()) {
 		cout << "Error occured, file cannot be open" << endl;
 	}
@@ -119,7 +159,7 @@ int main() {
 	string str2 = "sub $s4, $s5, $s6";
 	myFile << str1 << str2;
 	myFile.close();
-	mipsSimulator mips("myFile2.txt");
-	//mips.execute();
+	mipsSimulator mips("myFile3.txt");
+	// mips.execute();
 	return 0;
 }
