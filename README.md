84983f576263c70fb3a262b6b84f80464609f468
BobbaRuthvik <73455820+BobbaRuthvik@users.noreply.github.com>
2021-03-03 20:16:17 +0530

    Update 3/3/21 20:16

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # MIPS_byTechieTribe
+
+/***** Syntax Restrictions when compared to Qtspim *****/
+1. Space is must between registers used in instructions
+2. .data segment at the beginning of code and then .text
+3. Code in the next line after label
+<!-- 4. Label names should be in next line after .data -->    Check from Kishor
--- mips4.cpp
+++ mips4.cpp
@@ -3,6 +3,7 @@
 #include<fstream>
 #include<string>
 #include<map>
+#include<set>
 
 using namespace std;
 
@@ -12,11 +13,16 @@ class mipsSimulator {
 		map<string, int> map_instructions;
 		string commandsList[7] = {"add", "addi", "sub", "j", "bne", "lw", "sw"};			// Add, sub, jump, bne, lw, sw(modify accordingly)
 		vector<vector<string>> program;
+        set<string> data_types = {".word", ".asciiz", ".ascii"};
 		int maxLength;					// I think it's related to size limit mentioned(Please Check)
 		int programCounter;				// to store current line
 		string current_command;			// points to current command line
 		int TotalLines;				// total no. of commands in program(basically no. of lines)
 		// All commands can be made private but for now let's go with public to make life easy
+        map<string, vector<int>> map_word;
+        string memory[1024][2];  // arr[I][0] = "10100" arr[][1] = type
+        map<string, int> labels; // for storing address associated with label
+        int current_mem_address = 0;
 	public:
 		mipsSimulator(string filePath) {
 			TotalLines = 0;
@@ -40,6 +46,14 @@ class mipsSimulator {
                     }
                     cout << endl;
                 }
+
+                find_data();
+                cout << "curr_mem : " << current_mem_address << endl;
+                for(auto x : labels){
+                    cout << x.first << " " << x.second << endl;
+                }
+                Memdisplay();
+                
 			}
 			else {
 				cout << "File does not exist" << endl;
@@ -47,12 +61,20 @@ class mipsSimulator {
 			}
 		}
 
+        void Memdisplay(){
+            for(int i=0; i<current_mem_address; i++){
+                cout << memory[i][0] << " " << memory[i][1] << endl;
+            }
+        }
+
         vector<string> subVector(string s){
             vector<string> v;
             string temp = "";
             for(int i=0; i<s.size(); i++){
-                if(s[i] == ',' || s[i] == '$'){
-                    continue;
+                if(s[i] == ',' || s[i] == '$'){     // (resolved) if someone writes add $s0 $s1 $s2 still it works...
+                    temp+=' ';
+                    temp+=s[i];
+                    temp+=' ';
                 }
                 else{
                     temp += s[i];
@@ -65,16 +87,25 @@ class mipsSimulator {
         vector<string> getWords(string temp){
             vector<string> v;
             string word = "";
-            for (auto x : temp)  
+            for (int i=0; i<temp.length(); i++)  
             { 
-                if (x == ' ' || x == '\t') 
+                if(temp[i] == '"'){
+                    for (i=i+1;temp[i] != '"' && i<temp.length(); i++) {
+                         word = word + temp[i]; 
+                    }
+                    if(++i != temp.length()){
+                        cout << "error";  
+                        exit(1);                 // assumption : only one string can be initialized and that too at only end
+                    }
+                }
+                if (temp[i] == ' ' || temp[i] == '\t') 
                 {
                     if(word!="")
                         v.push_back(word); 
                     word = ""; 
                 } 
                 else { 
-                    word = word + x; 
+                    word = word + temp[i]; 
                 } 
             }
             if(word!="") 
@@ -83,8 +114,8 @@ class mipsSimulator {
         }
 
         void execute(){
-            int i = 0;
-            for(i=0; i<program.size(); i++){
+            int i;
+            for(i=programCounter; i<program.size(); i++){
                 if(program[i].size()!=0){
                     if(program[i][0] == ".text")
                         break;
@@ -117,13 +148,13 @@ class mipsSimulator {
                     string instruction = program[i][0];
                     // cout << instruction << endl;
                     if(instruction == "add"){
-                        add(program[i]);
+                        add(program[i], i);
                     }
                     else if(instruction == "addi"){
-                        addi(program[i]);
+                        addi(program[i], i);
                     }
                     else if(instruction == "sub"){
-                        sub(program[i]);
+                        sub(program[i], i);
                     }
                     else if(instruction == "j"){
                         i =  j(program[i]);
@@ -131,7 +162,7 @@ class mipsSimulator {
                     }
                     else if(instruction == "bne"){
                         int tempi;
-                        tempi = bne(program[i]);
+                        tempi = bne(program[i], i);
                         if(tempi != -1){
                             i = tempi;
                             programCounter = i;
@@ -164,59 +195,147 @@ class mipsSimulator {
             // string registerNames[32] = {"zero", "at", "v0", "v1", "a0", "a1", "a2", "a3", "t0", "t1", "t2", "t3", "t4", "t5", "t6", "t7", "s0", "s1", "s2", "s3", "s4", "s5", "s6", "s7", "t8", "t9", "k0", "k1", "gp", "sp", "fp", "ra"};
         }
 
-        void add(vector<string> v){
-            int a = map_instructions.find(v[2]) -> second;
-            int b = map_instructions.find(v[3]) -> second;
-            int c = a + b;
-            map_instructions.find(v[1]) -> second = c;
-            // programCounter++;
+        void add(vector<string> v, int pc){
+            if(v[1] == "$" && v[4] == "$" && v[7] == "$" && v[3] == "," && v[6] == ","){
+                int a = map_instructions.find(v[5]) -> second;
+                int b = map_instructions.find(v[8]) -> second;
+                int c = a + b;
+                map_instructions.find(v[2]) -> second = c;
+            }
+            else{
+                cout << "Error in line " << pc << "." << endl;
+                exit(1);
+            }
         }
 
-        void sub(vector<string> v){
-            int a = map_instructions.find(v[2]) -> second;
-            int b = map_instructions.find(v[3]) -> second;
-            int c = a - b;
-            map_instructions.find(v[1]) -> second = c;
-            // programCounter++;
+        void sub(vector<string> v, int pc){
+            if(v[1] == "$" && v[4] == "$" && v[7] == "$" && v[3] == "," && v[6] == ","){
+                int a = map_instructions.find(v[5]) -> second;
+                int b = map_instructions.find(v[8]) -> second;
+                int c = a - b;
+                map_instructions.find(v[2]) -> second = c;
+            }
+            else{
+                cout << "Error in line " << pc << "." << endl;
+                exit(1);
+            }
         }
 
-        void addi(vector<string> v){
-            int a = map_instructions.find(v[2]) -> second;
-            int b = stoi(v[3]);
-            int c = a + b;
-            map_instructions.find(v[1]) -> second = c;
-            // programCounter++;
+        void addi(vector<string> v, int pc){
+            if(v[1] == "$" && v[4] == "$" && v[3] == "," && v[6] == ","){
+                int a = map_instructions.find(v[5]) -> second;
+                int b = stoi(v[7]);
+                int c = a + b;
+                map_instructions.find(v[2]) -> second = c;
+            }
+            else{
+                cout << "Error in line " << pc << "." << endl;
+                exit(1);
+            }
         }
 
         int j(vector<string> v){
-            string label = v[1] + ":";
+            string label = v[1] + ":" + "\0";
             int i;
             for(i=0; i<program.size(); i++){
                 if(program[i].size() != 0){
-                    if(program[i][0] == label)
+                    if(program[i][0] == label){
                         break;
+                    }
                 }
             }
             return i;
         }
 
-        int bne(vector<string> v){
-            string label = v[3] + ":";
-            int a = map_instructions.find(v[1]) -> second;
-            int b = map_instructions.find(v[2]) -> second;
-            int i;
-            if(a != b){
-                for(i=0; i<program.size(); i++){
-                    if(program[i].size() != 0){
-                        if(program[i][0] == label)
-                            break;
+        int bne(vector<string> v, int pc){
+            if(v[1] == "$" && v[4] == "$" && v[3] == "," && v[6] == ","){
+                string label = v[7] + ":";
+                int a = map_instructions.find(v[2]) -> second;
+                int b = map_instructions.find(v[5]) -> second;
+                int i;
+                if(a != b){
+                    for(i=0; i<program.size(); i++){
+                        if(program[i].size() != 0){
+                            if(program[i][0] == label)
+                                break;
+                        }
                     }
                 }
+                else{
+                    i = -1;
+                }
+                return i;
             }
             else{
-                i = -1;
+                cout << "Error in line " << pc << "." << endl;
+                exit(1);
             }
-            return i;
+        }
+
+        void find_data(){
+            vector<vector<string>>::iterator itr;
+            for(itr = program.begin(); itr != program.end();  itr++,programCounter++){ 
+                if((*itr).size() == 0)
+                    continue;
+                else if((*itr)[0] == ".data"){
+                    for(itr++,programCounter++; (*itr).size() == 0 || data_types.find((*itr)[0])!=data_types.end() || ((*itr).size()>1 && data_types.find((*itr)[1])!=data_types.end()); itr++,programCounter++){
+                        if((*itr).size() == 0)
+                            continue;
+                        if((*itr)[1] == ".word" || (*itr)[0] == ".word"){
+                            
+                            int flag = 0;
+                            if((*itr)[1] == ".word"){
+                                flag = 1;
+                                int size = (*itr)[0].size();
+                                if((*itr)[0][size-1] == ':')
+                                    labels[(*itr)[0]] = current_mem_address;
+                                else{
+                                    cout << "error in line ...";
+                                    exit(1);
+                                }
+                            }
+                            for(int j=flag+1; j<(*itr).size(); j++){
+                                memory[current_mem_address][0] = (*itr)[j];  
+                                memory[current_mem_address][1] = "word";
+                                current_mem_address++; 
+                                if(++j < (*itr).size()){    // in QTspim if we write ',' or ' ', code is working, but now our code will work fine only if integers are seperated by ',' only
+                                    if((*itr)[j] == ",")
+                                        continue;
+                                    else {
+                                        cout << "error in line (No comma',')";
+                                        exit(1);
+                                    }
+                                }
+                                
+                            }
+                        }
+                        if((*itr)[1] == ".asciiz" || (*itr)[0] == ".asciiz"){
+                            // did only for one string initialization i.e .asciiz "abk" <-only one string
+                            int flag = 0;
+                            if((*itr).size() > 0 && (*itr)[1] == ".asciiz"){
+                                flag = 1;
+                                int size = (*itr)[0].size();
+                                if((*itr)[0][size-1] == ':')
+                                    labels[(*itr)[0]] = current_mem_address;
+                                else{
+                                    cout << "error in line ...";
+                                    exit(1);
+                                }
+                            }
+                            memory[current_mem_address][0] = (*itr)[flag+1];
+                            memory[current_mem_address][1] = "asciiz";   // how to differentiate asciiz and ascii
+                            current_mem_address++;
+                            
+                        }
+                    }
+                    cout << "PC : " << programCounter << endl;
+                    break; // since we assumed that only one .data can be written(if we remove break ,I think code will work fine if we have consecutive .data)
+                }
+                else{
+                    return; // since we assumed, .data can be written only at starting
+                }
+                programCounter++;
+            }
         }
 };
 
