0d67d39104db8d30d382497c96be2129e40d91b0
Kishor783 <70994761+Kishor783@users.noreply.github.com>
2021-03-10 00:28:20 +0530

    Update Bubbie_sort.s

--- Bubbie_sort.s
+++ Bubbie_sort.s
@@ -1,10 +1,12 @@
 .data
 array:  .word 10, 5, 18, 7, -9, 0, 56, 3, 7, 200, 8
+space:  .asciiz " "
 
 	.text
+	.globl main
 main:
 	la $s0, array
-	addi $s1, $zero, 10
+	li $s1, 10
 	j bubbleSort
 	
 
@@ -26,14 +28,32 @@ loop:
 
 jump:
 	addi $s0, $s0, 4
-	sub $s2, $s2, 1
+	sub $s2, $s2, 1 
 	j loop
 
 jmp:
-	sub $s1, $s1, 1
+	sub $s1, $s1, 1 
 	la $s0, array
 	j bubbleSort 
 	
 out:
+	li $s1, 11
+	j print
+	
+
+print:
+	beq $zero, $s1, jmpp
+	li $v0, 1
+	lw $a0, 0($s0)
+	syscall
+	li $v0, 4
+	la $a0, space
+	syscall
+	addi $s0, $s0, 4
+	sub $s1, $s1, 1
+	j print
+
+jmpp:
 	li $v0, 10
-    syscall
+	syscall
+
