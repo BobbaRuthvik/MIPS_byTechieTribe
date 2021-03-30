653037f50a3b083759f10d9aaa1da5cb682a679c
Kishor783 <70994761+Kishor783@users.noreply.github.com>
2021-03-30 17:00:02 +0530

    Update myFile3.s

--- myFile3.s
+++ myFile3.s
@@ -6,7 +6,7 @@ space:  .asciiz " "
 	.globl main
 main:
 	la $s0, array
-	li $s1, 10
+	addi $s1, $zero, 10
 	j bubbleSort
 	
 
@@ -37,16 +37,16 @@ jmp:
 	j bubbleSort 
 	
 out:
-	li $s1, 11
+	addi $s1, $zero, 11
 	j print
 	
 
 print:
 	beq $zero, $s1, jmpp
-	li $v0, 1
+	addi $v0, $zero, 1
 	lw $a0, 0($s0)
 	syscall
-	li $v0, 4
+	addi $v0, $zero, 4
 	la $a0, space
 	syscall
 	addi $s0, $s0, 4
@@ -54,6 +54,5 @@ print:
 	j print
 
 jmpp:
-	li $v0, 10
+	addi $v0, $zero, 10
 	syscall
-
